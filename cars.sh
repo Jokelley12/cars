e5135e784a38d2b34f3720875e6353f03c64e7fe
jokelley <jokelley@students.chapman.edu>
2025-11-03 20:27:00 -0800

    Fix read syntax and cleanup formatting

--- cars.sh
+++ cars.sh
@@ -5,13 +5,13 @@ while true ; do
    echo "1:Add a car to the list"
    echo "2:View the list"
    echo "3:Exit"
-   read -p "Choose an option (1-3): " choice
+   read -r -p "Choose an option (1-3): " choice
 
 case $choice in
         1)
-            read -p "Enter the make: " car_make
-            read -p "Enter the model: " car_model
-            read -p "Enter the year: " car_year
+            read -r -p "Enter the make: " car_make
+            read -r -p "Enter the model: " car_model
+            read -r -p "Enter the year: " car_year
             echo "$car_year $car_make $car_model" >> cars.txt
             echo "Car added successfully!"
             ;;
