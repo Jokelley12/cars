6dae32a76dbfe91e502112df1f6123fa3114eb89
jokelley <jokelley@students.chapman.edu>
2025-11-03 20:13:22 -0800

    Add working cars.sh script

---
+++ cars.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+# Joshua Kelley
+# cars.sh
+while true ; do
+   echo "1:Add a car to the list"
+   echo "2:View the list"
+   echo "3:Exit"
+   read -p "Choose an option (1-3): " choice
+
+case $choice in
+        1)
+            read -p "Enter the make: " car_make
+            read -p "Enter the model: " car_model
+            read -p "Enter the year: " car_year
+            echo "$car_year $car_make $car_model" >> cars.txt
+            echo "Car added successfully!"
+            ;;
+        2)
+            echo "List of cars:"
+            cat cars.txt
+            ;;
+        3)
+            echo "Goodbye!"
+            exit 0
+            ;;
+        
+    esac
+done
